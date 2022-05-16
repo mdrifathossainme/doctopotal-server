36109ae7216feaa1090d42ef5e6df132beb5ce73
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-16 11:36:25 +0600

    all

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const jwt = require('jsonwebtoken');
+const res = require('express/lib/response');
 const app=express()
 require('dotenv').config()
 const port=process.env.PORT||5000
@@ -13,11 +14,30 @@ app.use(cors())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wtkrj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
+const varyfyJTW=(req,res,next)=>{
+const authoHeader=req.headers.authorization;
+if(!authoHeader){
+    return res.status(401).send({massage:"you are UnAuthorized access"})
+
+}
+const token= authoHeader.split(' ')[1]
+jwt.verify(token,process.env.ACCES_TOKEN,function(err,decoded){
+    if(err){
+        return  res.status(401).send({message:"Forbidden access"})
+    }
+    req.decoded=decoded;
+    next()
+})
+}
+
 const run=async()=>{
         try{
             await client.connect();
             const dataCollection=client.db('doctor_protal').collection('services')
             const bookingCollection=client.db('doctor_protal').collection('booking')
+            const userCollection=client.db('doctor_protal').collection('user')
 
             app.get('/services',async(req,res)=>{
                 const quary={}
@@ -25,6 +45,61 @@ const run=async()=>{
                 const result=await cursor.toArray()
                 res.send(result)
             })
+            app.get('/user', varyfyJTW, async(req,res)=>{
+                const result=await userCollection.find().toArray()
+                res.send(result)
+            })
+
+
+
+
+            app.get('/booking',varyfyJTW,async(req,res)=>{
+                const patient=req.query.patient;
+            
+
+                const decodedEmail=req.decoded.email
+                if(patient===decodedEmail){
+                    const quary={patient:patient};
+                    const cursor=bookingCollection.find(quary)
+                    const result=await cursor.toArray()
+                    res.send(result)
+                }
+                else{
+                    return res.status(403).send({massage:"Forbidden access"})
+                }
+              
+            })
+            app.put('/user/admin/:email' , varyfyJTW,async(req,res)=>{
+                const email =req.params.email;
+                const requstEmail=req.decoded.email;
+                const reaustAuthor= await userCollection.findOne({email:requstEmail})
+                if(reaustAuthor.role==="admin"){
+                    const filter={email:email}
+                    const updateData={
+                        $set:{role:"admin"},
+                    }
+                    const result=await userCollection.updateOne(filter,updateData);
+                    res.send(result)
+                }
+                else{
+                    res.status(403).send({message:"forbidden"})
+                }
+               
+            })
+            app.put('/user/:email',async(req,res)=>{
+                const email =req.params.email;
+                const user= req.body;
+                const filter={email:email}
+                const options={upsert:true}
+                const updateData={
+                    $set:user,
+                }
+                const result=await userCollection.updateOne(filter,updateData,options);
+                const token=jwt.sign({email:email},process.env.ACCES_TOKEN,{expiresIn:"1h"})
+                res.send({result,token})
+            })
+
+            
 
             app.get('/available',async(req,res)=>{
                 const date=req.query.date
